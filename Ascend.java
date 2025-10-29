0bfb79e6eb5f583b08c1cc6bf80313c8d0d117e6
gilco2003 <cogil11cogil@gmail.com>
2025-10-29 14:35:49 +0200

    bill3

--- Ascend.java
+++ Ascend.java
@@ -3,5 +3,8 @@
 public class Ascend {
 	public static void main(String[] args) {
 		// Replace this comment with your code
+		int minRange, maxRange=9, rand1, rand2, rand3;
+		rand1= (int)(Math.random()*(double)maxRange);
+
 	}
 }
--- Bill3.java
+++ Bill3.java
@@ -2,7 +2,15 @@
 public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
-	    String name1 = args[0];
+	    //String name1 = args[0], name2 = args[1], name3 = args[2];
+		String name1, name2, name3 ;
+		//int price = Integer.parseInt(args[3]);
+		int price;
 	    // Replace this comment with the rest of your code   
+		name1="ben";
+		name2="gil";
+		name3="joe";
+		price=93;
+		System.out.println("Dear " + name1 + ", " +name2 + " ,and " +name3+": pay "+ price/3 + " Shekels each.");
 	}
 }
