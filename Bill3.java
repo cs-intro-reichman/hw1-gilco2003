0cd5c0e71b2f496e591af1047366f7a96b876f62
gilco2003 <cogil11cogil@gmail.com>
2025-10-29 18:31:46 +0200

    take1

--- Bill3.java
+++ Bill3.java
@@ -2,15 +2,10 @@
 public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
-	    //String name1 = args[0], name2 = args[1], name3 = args[2];
-		String name1, name2, name3 ;
-		//int price = Integer.parseInt(args[3]);
-		int price;
-	    // Replace this comment with the rest of your code   
-		name1="ben";
-		name2="gil";
-		name3="joe";
-		price=93;
-		System.out.println("Dear " + name1 + ", " +name2 + " ,and " +name3+": pay "+ price/3 + " Shekels each.");
+	    String name1 = args[0], name2 = args[1], name3 = args[2];
+		//Converting price to a number so we can use it for calculations.
+		double price = Double.parseDouble(args[3]);
+		//Please note that I used the ceil method with the division by 3 in the println string.
+		System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 +": pay " + Math.ceil(price/3) + " Shekels each.");
 	}
 }
--- FVCalc.java
+++ FVCalc.java
@@ -2,5 +2,11 @@
 public class FVCalc {
 	public static void main(String[] args){
 		// Replace this comment with your code
+		int currentValue = Integer.parseInt(args[0]), numberOfYears = Integer.parseInt(args[2]);
+		double interestRate = Double.parseDouble(args[1]), futureValue;
+		futureValue = currentValue * Math.pow((1.0 + interestRate), numberOfYears)/100;
+		System.out.println(futureValue);
+		System.out.println("After " + numberOfYears + " years, " + currentValue + "$ saved at " + interestRate + "% will yield " + (int)futureValue+"$");
+
 	}
 }
