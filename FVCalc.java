f5c12d3dd04188a672b8db50fdf51d409d33a878
gilco2003 <cogil11cogil@gmail.com>
2025-10-30 08:45:33 +0200

    //

--- FVCalc.java
+++ FVCalc.java
@@ -6,8 +6,11 @@ public class FVCalc {
 		double interestRate = Double.parseDouble(args[1]), futureValue;
 		//I divided the interest rate by a hundred because it represents a percantage.
 		futureValue = currentValue * Math.pow((1.0 + interestRate/100), numberOfYears);
-		//System.out.println("After " + numberOfYears + " years, $" + currentValue + " saved at " + interestRate + "% will yield $" + (int)futureValue);
-		System.out.println("After 2 years, 100$ saved at 10.0% will yield 121$");
+		/*System.out.print("After " + numberOfYears + " years, ");
+		System.out.print("$");
+		System.out.print( currentValue + " saved at " + interestRate + "% will yield" );*/
+		System.out.print("After " + numberOfYears + " years, $" + currentValue + " saved at " + interestRate + "% will yield $" + (int)futureValue);
+
 
 	
 
