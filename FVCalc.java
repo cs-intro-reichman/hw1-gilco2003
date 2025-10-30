77dccd311cc970da62ade5899cc1ec864bff3b05
gilco2003 <cogil11cogil@gmail.com>
2025-10-30 12:29:32 +0200

    comments

--- FVCalc.java
+++ FVCalc.java
@@ -7,10 +7,6 @@ public class FVCalc {
 		//I divided the interest rate by a hundred because it represents a percantage.
 		futureValue = currentValue * Math.pow((1.0 + interestRate/100), numberOfYears);
 		System.out.print("After " + numberOfYears + " years, $" + currentValue + " saved at " + interestRate + "% will yield $" + (int)futureValue);
-
-
-	
-
 	}
 }
 
