b82ec4a76a12089adc54dd653e56d9aefce54e8b
gilco2003 <cogil11cogil@gmail.com>
2025-10-30 17:57:20 +0200

    :)

--- FVCalc.java
+++ FVCalc.java
@@ -2,8 +2,10 @@
 public class FVCalc {
 	public static void main(String[] args){
 		//Initializing values according to the given parameters.
-		int currentValue = Integer.parseInt(args[0]), numberOfYears = Integer.parseInt(args[2]);
-		double interestRate = Double.parseDouble(args[1]), futureValue;
+		int currentValue = Integer.parseInt(args[0]);
+		int numberOfYears = Integer.parseInt(args[2]);
+		double interestRate = Double.parseDouble(args[1]);
+		double futureValue;
 		//I divided the interest rate by a hundred because it represents a percantage.
 		futureValue = currentValue * Math.pow((1.0 + interestRate/100), numberOfYears);
 		System.out.print("After " + numberOfYears + " years, $" + currentValue + " saved at " + interestRate + "% will yield $" + (int)futureValue);
--- NumWords.java
+++ NumWords.java
@@ -6,7 +6,9 @@ public class NumWords {
 	     As for the hunderds you simply divide by a hundred.
 		For the tens if you use module 100 you will get 10 * number of tens + 1 * number of units, 
 		all that's left is to reverse the equation so you get the numer of tens*/
-		int digitsNum = num % 10, tensNum = (num % 100 - digitsNum) / 10, hundredsNum = num / 100;
+		int digitsNum = num % 10;
+		int tensNum = (num % 100 - digitsNum) / 10;
+		int hundredsNum = num / 100;
 		System.out.println(hundredsNum+ " hundreds, " + tensNum + " tens, and " + digitsNum +  " ones.");
 	}
 }
